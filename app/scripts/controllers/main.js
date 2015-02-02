1d373435ca62539d1728dbcae8f7c6138b7ec049
DiegoGodoy <diego.godoypoce@alum.uca.es>
2015-02-02 10:57:54 +0100

    intento de slideshow

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,13 @@
-/*angular.module('resource', ['ngResource'])
-.factory('Clasificacion', function ($resource) {
+atletiblueApp.controller('MainCtrl', ['$scope','$resource', function ($scope,$resource) {
+
+      'use strict';
+
     var Clasificacion =
             $resource('https://api.mongolab.com/api/1/databases/atletiblue/collections/clasificacion/', {
             apiKey: 'ljDhb1Qm1DfhZGmKr8bZHlSzSbseJQA6',
             id: 'franmacias91'
             });
-})
-.controller('ClasificacionCtrl', function ($scope, Clasificacion) {
 
-    $scope.clasificacion = Clasificacion.query();
-
-    $scope.encabezado=[{nombre:'Equipo', inverso:'-Equipo'},
-                       {nombre:'PT', inverso:'-PT'},
-                       {nombre:'PJ', inverso:'-PJ'},
-                       {nombre:'PG', inverso:'-PG'},
-                       {nombre:'PE', inverso:'-PE'},
-                       {nombre:'PP', inverso:'-PP'},
-                       {nombre:'GF', inverso:'-GF'},
-                       {nombre:'GC', inverso:'-GC'},
-                       {nombre:'DG', inverso:'-DG'}];
-});*/
-atletiblueApp.controller('MainCtrl', ['$scope', function ($scope) {
-    
-      'use strict';
       //JSON en código para cargar la barra de navegación y sus rutas
     $scope.menu = [{ruta: '#', nombre: 'inicio'},
                    {ruta: '#historia', nombre: 'historia'},
@@ -35,23 +20,8 @@ atletiblueApp.controller('MainCtrl', ['$scope', function ($scope) {
                     $scope.ordenSeleccionado= orden;
                 };
 
-    $scope.handleEvent = function (e) {
-        if(e.type === 'mouseover')
-            $scope.data.fondoTabla= 'Blue';
-        else
-            $scope.data.fondoTabla='Beige';
-    }
+    $scope.clasificacion = Clasificacion.query();
 
-    //$scope.clasificacion = Clasificacion.query();
-
-    $scope.encabezado=[{nombre: 'Equipo', inverso: '-Equipo'},
-                       {nombre: 'PT', inverso: '-PT'},
-                       {nombre: 'PJ', inverso: '-PJ'},
-                       {nombre: 'PG', inverso: '-PG'},
-                       {nombre: 'PE', inverso: '-PE'},
-                       {nombre: 'PP', inverso: '-PP'},
-                       {nombre: 'GF', inverso: '-GF'},
-                       {nombre: 'GC', inverso: '-GC'},
-                       {nombre: 'DG', inverso: '-DG'}];
+    $scope.encabezado=['Equipo', 'PT', 'PJ', 'PG', 'PE', 'PP', 'GF', 'GC', 'DG'];
 
 }]);
--- app/scripts/controllers/plantilla.js
+++ app/scripts/controllers/plantilla.js
@@ -2,9 +2,39 @@ atletiblueApp.controller('PlantillaCtrl', ['$scope', function ($scope) {
 
     'use strict';
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    /*var navegar = function () {
+        var elemento = document.getElementById('galeria').getElementsByTagName('li');
+        var pos = 0;
+        for (var i=0; i<elemento.length; i++) {
+            if (elemento[i].className == "selected") {
+                elemento[i].className = "noselected";
+                pos = i;
+            }
+        }*/
+        /** Pulsar Anterior **/
+        /*if (this.id == "prev" && pos > 0) {
+            elemento[pos-1].className = "selected";
+        } else if (this.id == "prev") {
+            elemento[elemento.length-1].className = "selected";
+        }*/
+
+        /** Pulsar Siguiente **/
+       /* if (this.id == "next" && pos < elemento.length-1) {
+            elemento[pos+1].className = "selected";
+        } else if (this.id == "next") {
+            elemento[0].className = "selected";
+        }
+        return false;
+    };
+
+    var desplazar = function () {
+        var nav = document.querySelectorAll(".nav");
+        for (var i=0; i<nav.length; i++) {
+            nav[i].onclick = navegar;
+        }
+    };
+
+    window.onload = function (){
+        new desplazar();
+    };*/
 }]);
