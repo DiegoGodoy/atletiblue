9f2e9067ece9309739e52b311e30bde992accdb6
franmacias91 <franmacias91@gmail.com>
2015-02-05 20:51:15 +0000

    final grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,17 +30,20 @@ module.exports = function (grunt) {
             dest: "app/scripts/juntosComprimidos.js"
             
           },
-          css:
-          {
-            src: ["app/views/noticias/noticia1.html","app/views/noticias/noticia1.html","app/views/noticias/noticia1.html","app/views/noticias/noticia1.html",
-                  "app/views/noticias/noticia1.html","app/views/noticias/noticia1.html","app/views/noticias/noticia1.html","app/views/noticias/noticia1.html",
-                  "app/views/noticias/noticia1.html","app/views/noticias/noticia1.html","app/views/noticias/noticia1.html"],
-            dest: "app/scripts/juntosComprimidos.js"
-          },
           html:
           {
-            src: "app/scripts/juntos.js",
-            dest: "app/scripts/juntosComprimidos.js"
+            src: ["app/views/noticias/noticia1.html","app/views/noticias/noticia2.html","app/views/noticias/noticia3.html","app/views/noticias/noticia4.html",
+                  "app/views/noticias/noticia5.html","app/views/contacto.html","app/views/historia.html","app/views/main.html",
+                  "app/views/noticias.html","app/views/plantilla.html","app/views/index.html"],
+            dest: ["app/views/Comprimido/noticia1Comprimido.html","app/views/Comprimido/noticia2Comprimido.html","app/views/Comprimido/noticia3Comprimido.html",
+                  "app/views/Comprimido/noticia4Comprimido.html","app/views/Comprimido/noticia5Comprimido.html","app/views/Comprimido/contactoComprimido.html",
+                  "app/views/Comprimido/historiaComprimido.html","app/views/Comprimido/mainComprimido.html","app/views/Comprimido/noticiasComprimido.html",
+                  "app/views/Comprimido/plantillaComprimido.html","app/views/Comprimido/indexComprimido.html"]
+          },
+          css:
+          {
+            src: "app/styles/styles.css",
+            dest: "app/styles/stylesComprimido.css"
           }
         }
 });
